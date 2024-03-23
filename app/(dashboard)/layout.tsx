af0bd1742a5f896695733f729d3b915e8b35a9bf
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-03-23 11:06:46 +0100

    feat: layout and signout button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,17 @@
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+        <div className="flex gap-4 items-center">
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
